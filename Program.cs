6c94b9aecd4fbb3ad38d3478ce829a426f1470f2
Thedorus89 <theodoredanang@gmail.com>
2024-07-24 15:37:24 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class MergeNames
+{
+    public static string[] UniqueNames(string[] names1, string[] names2)
+    {
+        HashSet<string> uniqueNamesSet = new HashSet<string>();
+
+        foreach (var name in names1)
+        {
+            uniqueNamesSet.Add(name);
+        }
+
+        foreach (var name in names2)
+        {
+            uniqueNamesSet.Add(name);
+        }
+
+        return uniqueNamesSet.ToArray();
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+        string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+        Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}
